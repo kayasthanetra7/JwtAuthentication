03b211f47015ea63c1950151aaf0040ebff44d5c
Netra Kayastha <netrakayastha@nexsystech.com>
2021-08-06 16:21:02 -0400

    Add project files.

---
+++ Controllers/JwtController.cs
@@ -0,0 +1,21 @@
+﻿using JwtAuthentication.Helper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtAuthentication.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class JwtController : ControllerBase
+    {
+        [HttpGet]
+       public IActionResult Jwt()
+        {
+            return new ObjectResult(JwtTokenHelper.GenerateToken());
+        }
+    }
+}
---
+++ Helper/JwtTokenHelper.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtAuthentication.Helper
+{
+    public class JwtTokenHelper
+    {
+        private const string Secret_Key = "this is my custom Secret key for authnetication and what not";
+
+        public static readonly SymmetricSecurityKey Signing_key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Secret_Key));
+
+        public static string GenerateToken()
+        {
+            var credentials = new Microsoft.IdentityModel.Tokens.SigningCredentials
+                (Signing_key, SecurityAlgorithms.HmacSha256);
+
+            var header = new JwtHeader(credentials);
+
+            DateTime expiry = DateTime.UtcNow.AddMinutes(12);
+
+            var ts = (int)(expiry - new DateTime(1970, 1, 1)).TotalSeconds;
+
+            var payload = new JwtPayload
+            {
+                {"name", "abc" },
+                {"exp", ts },
+                {"iss", "https://localhost:44365/" },
+                {"aud", "https://localhost:44365/" }
+            };
+
+            var secToken = new JwtSecurityToken(header, payload);
+
+            var handler = new JwtSecurityTokenHandler();
+
+            var token = handler.WriteToken(secToken);
+
+            Console.WriteLine(token);
+            Console.WriteLine("consume token");
+
+            return token;
+
+        }
+    }
+}
